640c1d4b6000294ace9dcc7cd5d2b3c5336219b5
Evgeny Odintsov <ev1genial@gmail.com>
2023-11-08 21:56:35 +0300

    Add calculation fo X variable

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <stdexcept>
 #include <map>
 #include "stack.cpp"
-#include "queue.cpp"
 
 
 std::map<std::string, int> operationPriority = {
@@ -30,28 +29,17 @@ bool is_oper(const std::string &oper) {
 }
 
 bool is_digits(const std::string &str) {
-    for (char ch: str) {
-        if (!std::isdigit(ch)) return false;
-    }
+    for (char ch: str)
+        if (!(std::isdigit(ch) || ch == '.'))
+            return false;
     return true;
 }
 
-void check_digits(std::string &digits, std::vector<std::string> &result) {
-    if (!digits.empty()) {
-        result.push_back(digits);
-        digits.clear();
-    }
-}
-
-void check_chars(std::string &chars, std::vector<std::string> &result) {
-    if (!chars.empty()) {
-        if (is_func(chars)) {
-            result.push_back(chars);
-            chars.clear();
-        } else {
-            throw std::invalid_argument("Invalid function: " + chars);
-        }
-    }
+bool find_x(const std::vector<std::string> &data) {
+    for (const auto &token: data)
+        if (token == "x")
+            return true;
+    return false;
 }
 
 std::vector<std::string> tokenization(const std::string &input) {
@@ -60,14 +48,34 @@ std::vector<std::string> tokenization(const std::string &input) {
 
     for (char symbol: input) {
         if (symbol == '(' || symbol == ')' || is_oper(std::string(1, symbol)) || symbol == 'x') {
-            check_digits(digits, tokens);
-            check_chars(chars, tokens);
+            if (!digits.empty()) {
+                tokens.push_back(digits);
+                digits.clear();
+            }
+            if (!chars.empty()) {
+                if (is_func(chars)) {
+                    tokens.push_back(chars);
+                    chars.clear();
+                } else {
+                    throw std::invalid_argument("Invalid function: " + chars);
+                }
+            }
             tokens.emplace_back(1, symbol);
         } else if (std::isdigit(symbol) || symbol == '.') {
-            check_chars(chars, tokens);
+            if (!chars.empty()) {
+                if (is_func(chars)) {
+                    tokens.push_back(chars);
+                    chars.clear();
+                } else {
+                    throw std::invalid_argument("Invalid function: " + chars);
+                }
+            }
             digits += symbol;
         } else if (std::isalpha(symbol)) {
-            check_digits(digits, tokens);
+            if (!digits.empty()) {
+                tokens.push_back(digits);
+                digits.clear();
+            }
             chars += symbol;
         } else if (std::isspace(symbol)) {
             continue;
@@ -76,8 +84,16 @@ std::vector<std::string> tokenization(const std::string &input) {
         }
     }
 
-    check_chars(chars, tokens);
-    check_digits(digits, tokens);
+    if (!digits.empty()) {
+        tokens.push_back(digits);
+    }
+    if (!chars.empty()) {
+        if (is_func(chars)) {
+            tokens.push_back(chars);
+        } else {
+            throw std::runtime_error("Invalid data!");
+        }
+    }
 
     return tokens;
 }
@@ -93,48 +109,54 @@ void print(const std::vector<std::string> &postfixTokens, const Stack<std::strin
 }
 
 std::vector<std::string> infix_to_postfix(const std::vector<std::string> &infixTokens) {
-    Stack<std::string> operators;
+    Stack<std::string> operatorStack;
     std::vector<std::string> postfixTokens;
+
     for (const std::string &token: infixTokens) {
         if (is_digits(token) || token == "x") {
             postfixTokens.push_back(token);
         } else if (is_func(token)) {
-            operators.push(token);
+            operatorStack.push(token);
         } else if (is_oper(token)) {
-            while (!operators.is_empty() && is_oper(operators.top()) &&
-                   operationPriority[operators.top()] >= operationPriority[token]) {
-                postfixTokens.push_back(operators.top());
-                operators.pop();
+            if (token == "-" && (operatorStack.is_empty() || operatorStack.top() == "(")) {
+                postfixTokens.emplace_back("0");
+                operatorStack.push(token);
+            } else {
+                while (!operatorStack.is_empty() && is_oper(operatorStack.top()) &&
+                       operationPriority[operatorStack.top()] >= operationPriority[token]) {
+                    postfixTokens.push_back(operatorStack.top());
+                    operatorStack.pop();
+                }
+                operatorStack.push(token);
             }
-            operators.push(token);
         } else if (token == "(") {
-            operators.push(token);
+            operatorStack.push(token);
         } else if (token == ")") {
-            while (operators.top() != "(") {
-                if (operators.is_empty()) {
+            while (operatorStack.top() != "(") {
+                if (operatorStack.is_empty()) {
                     throw std::runtime_error("There are mismatched parentheses.");
                 }
-                postfixTokens.push_back(operators.top());
-                operators.pop();
+                postfixTokens.push_back(operatorStack.top());
+                operatorStack.pop();
             }
-            if (operators.top() == "(") {
-                operators.pop();
+            if (operatorStack.top() == "(") {
+                operatorStack.pop();
             }
-            if (is_func(operators.top())) {
-                postfixTokens.push_back(operators.top());
-                operators.pop();
+            if (is_func(operatorStack.top())) {
+                postfixTokens.push_back(operatorStack.top());
+                operatorStack.pop();
             }
         }
-        print(postfixTokens, operators);
+        print(postfixTokens, operatorStack);
     }
 
-    while (!operators.is_empty()) {
-        if (operators.top() == "(") {
+    while (!operatorStack.is_empty()) {
+        if (operatorStack.top() == "(") {
             throw std::runtime_error("There are mismatched parentheses.");
         }
-        postfixTokens.push_back(operators.top());
-        operators.pop();
-        print(postfixTokens, operators);
+        postfixTokens.push_back(operatorStack.top());
+        operatorStack.pop();
+        print(postfixTokens, operatorStack);
     }
 
     return postfixTokens;
@@ -142,46 +164,54 @@ std::vector<std::string> infix_to_postfix(const std::vector<std::string> &infixT
 
 double calculate(const std::vector<std::string> &postfixTokens) {
     Stack<double> numbers;
+
     for (const std::string &token: postfixTokens) {
         if (is_digits(token)) {
             numbers.push(std::stod(token));
         } else {
-            double result = 0.0;
-            if (is_oper(token)) {
-                if (numbers.get_size() < 2) {
-                    throw std::runtime_error("Not enough operands for the operator: " + token);
-                }
-                double b = numbers.top();
-                numbers.pop();
-                double a = numbers.top();
-                numbers.pop();
-                if (token == "+")
-                    result = a + b;
-                else if (token == "-")
-                    result = a - b;
-                else if (token == "*")
-                    result = a * b;
-                else if (token == "/") {
-                    if (b == 0) {
-                        throw std::runtime_error("Division by zero.");
+            double result = 0;
+
+            try {
+                if (is_oper(token)) {
+                    if (numbers.get_size() < 2) {
+                        throw std::runtime_error("Not enough operands for the operator: " + token);
                     }
-                    result = a / b;
-                } else if (token == "^")
-                    result = std::pow(a, b);
-            } else {
-                if (numbers.get_size() < 1) {
-                    throw std::runtime_error("Not enough operands for the operator: " + token);
+
+                    double b = numbers.top();
+                    numbers.pop();
+                    double a = numbers.top();
+                    numbers.pop();
+
+                    if (token == "+")
+                        result = a + b;
+                    else if (token == "-")
+                        result = a - b;
+                    else if (token == "*")
+                        result = a * b;
+                    else if (token == "/") {
+                        result = a / b;
+                    } else if (token == "^")
+                        result = std::pow(a, b);
+                } else {
+                    if (numbers.get_size() < 1) {
+                        throw std::runtime_error("Not enough operands for the operator: " + token);
+                    }
+
+                    double a = numbers.top();
+                    numbers.pop();
+
+                    if (token == "sin")
+                        result = std::sin(a);
+                    else if (token == "cos")
+                        result = std::cos(a);
+                    else if (token == "tan")
+                        result = std::tan(a);
+                    else if (token == "ctg")
+                        result = 1 / std::tan(a);
                 }
-                double a = numbers.top();
-                numbers.pop();
-                if (token == "sin")
-                    result = std::sin(a);
-                else if (token == "cos")
-                    result = std::cos(a);
-                else if (token == "tan")
-                    result = std::tan(a);
-                else if (token == "ctg")
-                    result = 1.0 / std::tan(a);
+            } catch (const std::invalid_argument &ex) {
+                std::cerr << ex.what() << std::endl;
+                return 1;
             }
 
             numbers.push(result);
@@ -195,19 +225,46 @@ double calculate(const std::vector<std::string> &postfixTokens) {
     return numbers.top();
 }
 
-
 int main() {
-    std::string inputData = "8/2-7/0";
+    std::string inputData;
+    std::cout << "Enter a mathematical expression:" << std::endl;
+    std::cin >> inputData;
+
+//    std::string inputData = "2.5 + 3 / x";
     std::vector<std::string> tokens = tokenization(inputData);
     std::vector<std::string> postfixTokens = infix_to_postfix(tokens);
-    std::cout << "Reverse Polish: ";
-    for (const std::string &token: postfixTokens) {
-        std::cout << token << " ";
+
+    if (find_x(postfixTokens)) {
+        double xmin, xmax, h;
+        std::cout << "Input xmin, xmax and h:" << std::endl;
+        std::cin >> xmin >> xmax >> h;
+
+        if (xmin >= xmax || h >= xmax - xmin) {
+            std::cerr << "'Xmin' should be less than 'Xmax', and 'h' should be less than their difference."
+                      << std::endl;
+            return 1;
+        }
+
+        double x = xmin;
+        while (x < xmax) {
+            std::vector<std::string> opz = postfixTokens;
+            for (std::string &token: opz) {
+                if (token == "x") {
+                    token = std::to_string(x);
+                }
+            }
+
+            std::cout << "For 'x' = " << x << "\tFunction = " << calculate(opz)
+                      << std::endl;
+            x += h;
+        }
+    } else {
+        std::cout << "Reverse Polish Notation: ";
+        for (const std::string &token: postfixTokens) {
+            std::cout << token << " ";
+        }
+        std::cout << "\nResult: " << calculate(postfixTokens) << std::endl;
     }
-    std::cout << std::endl;
-    std::cout << "Result: " << calculate(postfixTokens) << std::endl;
 
     return 0;
 }
-
-
